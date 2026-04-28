15867a09081156f3f1e064706b9fd3c07d7d898c
chiscookeke11 <chiscookeke11@gmail.com>
2026-04-28 05:22:14 +0100

    chore: configure development script with ts-node-dev

---
+++ src/utils/logic.ts
@@ -0,0 +1,20 @@
+
+
+
+
+export const isValidCardNumber = (cardNumber: string): boolean => {
+
+    const digits = cardNumber.replace(/\D/g, "").split("").map(Number).reverse();
+
+
+    const sum = digits.reduce((acc, digit, index) => {
+        if (index % 2 === 1) {
+            let doubled = digit * 2;
+            if (doubled > 9) doubled -= 9;
+            return acc + doubled;
+        }
+        return acc + digit;
+    }, 0)
+
+    return sum % 10 === 0
+}
