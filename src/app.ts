661433e79adba12fafbe35824522fb5c805368f5
chiscookeke11 <chiscookeke11@gmail.com>
2026-04-28 05:38:23 +0100

    test: add unit tests for card validation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from "express"
-
+import cardRoutes from "./routes/card.routes"
 
 
 const app = express()
 
 app.use(express.json())
 
-
+app.use("/api", cardRoutes);
 
 app.listen(3000, () => {
     console.log("Server running on port 3000")
---
+++ tests/card.test.ts
@@ -0,0 +1,11 @@
+import { isValidCardNumber } from "../src/utils/logic.js"
+
+describe("Card validation", () => {
+  it("should validate a correct card", () => {
+    expect(isValidCardNumber("4539578763621486")).toBe(true);
+  });
+
+  it("should reject invalid card", () => {
+    expect(isValidCardNumber("1234567890123456")).toBe(false);
+  });
+});
