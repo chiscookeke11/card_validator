5644948032fc8c7e41074007eda276b25a13f3a8
chiscookeke11 <chiscookeke11@gmail.com>
2026-04-28 05:26:52 +0100

    feat: add card validation controller

---
+++ src/controllers/card.controllers.ts
@@ -0,0 +1,30 @@
+import type { Request, Response } from "express";
+import { isValidCardNumber } from "../utils/logic.js";
+
+export const validateCard = (req: Request, res: Response) => {
+    const { cardNumber } = req.body;
+
+    if (!cardNumber) {
+        return res.status(400).json({
+            success: false,
+            message: "cardNumber is required"
+        });
+    }
+
+    if (typeof cardNumber !== "string") {
+        return res.status(400).json({
+            success: false,
+            message: "cardNumber must be a string"
+        });
+    }
+
+    const isValid = isValidCardNumber(cardNumber);
+
+    return res.status(200).json({
+        success: true,
+        data: {
+            cardNumber,
+            isValid
+        }
+    });
+};
